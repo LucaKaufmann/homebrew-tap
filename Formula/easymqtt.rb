baf34c5d6711fc263f8864be0a8b8ddfb1686b0a
Luca Kaufmann <lucakaufmann92@gmail.com>
2026-04-22 11:06:17 +0300

    easymqtt 0.1.0

--- Formula/easymqtt.rb
+++ Formula/easymqtt.rb
@@ -3,7 +3,7 @@ class Easymqtt < Formula
   homepage "https://github.com/LucaKaufmann/easymqtt-cli-releases"
   url "https://github.com/LucaKaufmann/easymqtt-cli-releases/releases/download/v0.1.0/easymqtt-macos-universal.tar.gz"
   version "0.1.0"
-  sha256 "acb4951048d3ae5189eebb0879b51c18965f005fd4fb059328c794a92b5c7a4c"
+  sha256 "2d25c831e903bac26158b87fb9d1525f73206180d6578a53da08cd1f8e4da6ee"
   license "Proprietary"
 
   def install
